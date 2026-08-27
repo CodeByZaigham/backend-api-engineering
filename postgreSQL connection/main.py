aec70fbcacee7c9f25494d89e83655db138f9799
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-08-28 00:51:57 +0500

    quiz application completed

--- postgreSQL connection/main.py
+++ postgreSQL connection/main.py
@@ -19,3 +19,21 @@ def get_db():
 def health():
      return {"message":"test route working"}
 
+@app.post("/questions")
+def add_questions(input:Question,db:Session=Depends(get_db)):
+     db_question=Questions(question=input.text)
+     db.add(db_question)
+     db.commit()
+     db.refresh(db_question)
+     for choice in input.choices:
+          db_choice=Choices(text=choice.text , is_true=choice.is_true , question_id=db_question.id)
+          db.add(db_choice)
+     db.commit()
+     # db.refresh()
+
+@app.get("/get_questions/{id}")
+def show_quiz(id:int,db:Session=Depends(get_db)):
+     question=db.query(Questions).filter(Questions.id==id).first()
+     if not question: raise HTTPException(status_code=404,detail="no question found for the question id!")
+     choice=db.query(Choices).filter(Choices.question_id==id).all()
+     return {"question: ":question , "choices: ":choice}
