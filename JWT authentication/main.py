13765e0fa0ea9f98fbc9e56fbc2473c4f7539225
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-08-29 00:25:15 +0500

    account creation and login endpoints added. JWT token generated on login

--- JWT authentication/main.py
+++ JWT authentication/main.py
@@ -16,5 +16,35 @@ app=FastAPI(title="Login System")
 def health():
      return {"message":"test route working"}
 
+@app.post("/createuser")
+def create_user(data:createuser,db:Session=Depends(get_db)):
+     hashed_password=hash_password(data.password)
+     new_user=User(
+          username=data.username,
+          email=data.email,
+          age=data.age,
+          disabled=data.disabled,
+          role=data.role,
+          hash_password=hashed_password
+     )
+     db.add(new_user)
+     db.commit()
+     db.refresh(new_user)
+     return {"message":"Account created Successfully!"}
 
+@app.post("/login",response_model=token)
+def userlogin(data:OAuth2PasswordRequestForm=Depends() , db:Session=Depends(get_db)):
+     user=db.query(User).filter(User.username==data.username)
+     for u in user:
+          if verify_password(data.password,u.hash_password):
+               user=u
+     if not user: raise HTTPException(
+          status_code=status.HTTP_401_UNAUTHORIZED,
+          detail="Incorrect username or password", 
+          headers={"WWW-Authenticate": "Bearer"}
+     )
+
+     token_expiry=timedelta(minutes=30)          
+     token=create_jwt_token(data={"sub":user.userid,"role":user.role},expiry=token_expiry)
+     return {"access_token":token , "token_type":"bearer"}
           
