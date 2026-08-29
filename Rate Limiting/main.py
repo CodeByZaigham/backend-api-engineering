107a81ce590722f07cc8c030d672604b07d5f5a6
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-08-30 03:11:01 +0500

    Rate limiting concept on a test route demonstrated

--- Rate Limiting/main.py
+++ Rate Limiting/main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Request , Response
+from fastapi.responses import JSONResponse
+from slowapi.middleware import SlowAPIMiddleware
+from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
+from limiter import limiter
 
-app=FastAPI(title="Rate Limiting")
+app=FastAPI(title="Rate limiting example")
+app.state.limiter=limiter
+
+app.add_exception_handler(
+     RateLimitExceeded,
+     _rate_limit_exceeded_handler
+     # lambda request,exc :JSONResponse(
+     #      status_code=429,
+     #      content={
+     #           "detail":"Rate Limit Exceeded"
+     #      }
+     # )
+)
+
+app.add_middleware(SlowAPIMiddleware)
+
 
 @app.get("/",response_model=dict)
-def health():
+@limiter.limit("2/minute")
+def health(request:Request,response:Response):
      return {"message":"test route working"}
