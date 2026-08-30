48ce7703daac1dd35b4605665d51bc54e1ced8fe
CodeByZaigham <zaigham.shah2004@gmail.com>
2026-08-31 01:00:09 +0500

    completed exception handling topic with global and custom handling

--- Exception Handling/main.py
+++ Exception Handling/main.py
@@ -11,3 +11,35 @@ from exceptions.handlers import (
 
 app = FastAPI()
 
+app.add_exception_handler(
+    UserNotFoundException,
+    user_not_found_handler
+)
+
+app.add_exception_handler(
+    DatabaseException,
+    database_exception_handler
+)
+
+app.add_exception_handler(
+     HTTPException,
+     http_exception_handler
+)
+
+@app.get("/users/{user_id}")
+def users(user_id:str):
+     if user_id!="0026": raise UserNotFoundException(user_id)
+     return {"user":user_id}
+
+
+@app.get("/test-database")
+def test_database():
+     raise DatabaseException
+
+
+@app.get("/test-http")
+def test_http():
+     raise HTTPException(
+          status_code=404,
+          detail="This resource does not exist"
+     )
